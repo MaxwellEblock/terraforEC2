54739fcbb4652b12d031b095ecec542eea83921a
MaxwellEblock <104592359+MaxwellEblock@users.noreply.github.com>
2023-05-03 15:02:22 -0500

    terraform-ec2-modules-only

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type = "t2.micro"
   key_name      = var.key_name
   vpc_security_group_ids = [var.security_group_id]
-  subnet_id     = var.subnet_ids[0]
+  subnet_id     = var.subnet_ids
 
   tags = {
     Name = "free-ec2-instance"
